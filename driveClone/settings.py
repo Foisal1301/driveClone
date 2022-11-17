327bb41d2855ca375c6030f7b7f43b6da041bc95
Mohammad Foisal <abbasnurul805@gmail.com>
2022-11-17 13:08:39 +0600

    initial commit

--- driveClone/settings.py
+++ driveClone/settings.py
@@ -122,9 +122,9 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+# )
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
